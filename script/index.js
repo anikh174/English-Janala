e40b4f64b78396c5a03d12226a45aad773b8501f
anikh174 <hossainanik174@gmail.com>
2026-03-05 21:26:18 +0600

    added card section and fix some errors

---
+++ script/index.js
@@ -0,0 +1,68 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLessons(json.data));
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+        <div class="text-center col-span-full space-y-6 font-bangla">
+        <img src="./assets/alert-error.png" alt="" class="w-[96px] h-[96px] mx-auto">
+        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+        `;
+    return;
+  }
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">${word.word? word.word : "শব্দ পাওয়া যায়নি" }</h2>
+        <p class="font-semibold">Meaning/Pronunciation</p>
+
+        <div class="font-bangla text-2xl font-medium">
+          ${word.meaning? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}
+        </div>
+
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
+        `;
+
+    wordContainer.append(card);
+  });
+};
+
+const displayLessons = (lessons) => {
+  //1. get the container and empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  //2. get into every lessons
+  for (let lesson of lessons) {
+    //3. create element
+    console.log(lesson);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+    <button onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i></i>Lesson - ${lesson.level_no}</button>
+    `;
+    //4. append into container
+    levelContainer.append(btnDiv);
+  }
+};
+
+loadLessons();
