a3e7e80a795a5f34ed1f3a649063df15aac03d67
anikh174 <hossainanik174@gmail.com>
2026-03-13 19:36:51 +0600

    added modal functionality and info

--- script/index.js
+++ script/index.js
@@ -1,3 +1,8 @@
+const createElement = (arr) =>{
+  const htmlElements = arr.map(el => `<span class="btn">${el}</span>`);
+  return (htmlElements.join(" "));
+}
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
@@ -20,6 +25,35 @@ const loadLevelWord = (id) => {
       displayLevelWord(data.data)
     });
 };
+
+const loadWordDetail=async(id)=>{
+  const url=`https://openapi.programming-hero.com/api/word/${id}`
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+}
+
+const displayWordDetails=(word)=>{
+  const detailsBox = document.getElementById('details-container');
+  detailsBox.innerHTML = `
+    <div class="">
+        <h2 class="text-2xl font-bold">${word.word} (  <i class="fa-solid fa-microphone-lines"></i>   :${word.pronunciation})</h2>
+      </div>
+      <div>
+        <h2 class="font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+      <div>
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+      <div>
+        <h2 class="font-bold">synonym</h2>
+        <div class>${createElement(word.synonyms)}</div>
+      </div>
+  `;
+  document.getElementById('word_modal').showModal();
+}
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
@@ -47,7 +81,7 @@ const displayLevelWord = (words) => {
         </div>
 
         <div class="flex justify-between items-center">
-          <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
